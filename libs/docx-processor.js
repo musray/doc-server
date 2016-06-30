51fedda555985188f6ee3cf60a6b29fc72ab9ee8
musray <churui.ctec@gmail.com>
2016-06-30 09:04:30 +0800

    complete with docx-processor and its testing

--- libs/docx-processor.js
+++ libs/docx-processor.js
@@ -3,6 +3,27 @@ var Docxtemplater = require('docxtemplater');
 var path = require('path');
 var fs = require('fs');
 
+/*
+ * <<<***** The Usage *****>>>
+ * 1. dependencies: docxtemplater
+ * https://github.com/open-xml-templating/docxtemplater
+ *
+ * 1. Arguments
+ * This module accept two arguments
+ * 'template': MUST be the full path of a template docx file
+ * 'dataSet': the data structure that containing the key: value pairs
+ * to be filled in the docx template
+ *
+ * 2. Returned object
+ * The parsed docx content will be returned
+ * in a node buffer, which variable name is 'buf'.
+ *
+ * 3. Create the rendered docx file
+ * This buffer can be write to file system 
+ * by fs.writeFileSync(filename, buf);
+ *
+ */
+
 module.exports = function ( template, dataSet ) {
   var tplContent = fs.readFileSync( template, 'binary' );
   var output = new Docxtemplater(tplContent);
@@ -10,24 +31,5 @@ module.exports = function ( template, dataSet ) {
   output.render();
   var buf = output.getZip()
                   .generate({ type: 'nodebuffer' });
-  var outputFile = __dirname + '/'
-                   + dataSet.t + path.extname(content);
-  fs.writeFileSync(outputFile, buf);
-  console.log(path.basename(outputFile) + ' is generated!');
-  return outputFile;
+  return buf;
 };
-    // if ( extensionName == '.docx' ) {
-    //   var output = new Docxtemplater(content);
-    //   output.setData(dataSet);
-    //   output.render();
-    //   var buf = output.getZip()
-    //                  .generate({ type: 'nodebuffer' });
-    //
-    //   var outputFile = __dirname + '/output/' 
-    //                  + doc + '_' + dataSet.t 
-    //                  + path.extname(fileType[doc]);
-    //
-    //   fs.writeFileSync(outputFile, buf);
-    //   console.log(path.basename(outputFile) + 'is generated!');
-    //   generatedFiles.push(outputFile);
-    // }
--- tests/test-docx-processor.js
+++ tests/test-docx-processor.js
@@ -1,9 +1,13 @@
 'use strict'
+var fs = require('fs');
+var path = require('path');
 var processor = require('../libs/docx-processor');
 var dataSet = {
-  't': 'This is title',
+  't': '二手玫瑰 伎俩 歌词',
   'r': 'This is revision'
 };
 var template = __dirname + '/test-tpl.docx'
-var name = processor(template, dataSet);
-console.log(name);
+var docBuf = processor(template, dataSet);
+var outputFileName = __dirname + '/' + dataSet.t + path.extname(template);
+fs.writeFileSync(outputFileName, docBuf);
+console.log('File is saved!');
