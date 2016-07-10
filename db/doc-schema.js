dd68393d6afb262d72d31b1b5fc206ec1179daec
musray <churui.ctec@gmail.com>
2016-07-10 08:05:37 +0800

    still working on database and api, 20%...

--- db/doc-schema.js
+++ db/doc-schema.js
@@ -1,11 +1,28 @@
 'use strict'
 const mongoose = require('mongoose');
 
-let docSchema = {
-  _id: {
-    type: String,
-    ref: ''
-  },
+let RevisionSchema = new mongoose.Schema({
+  rev: String,
+  status: String,
+  date: Date,
+  draft: String,
+  check: String,
+  review: String,
+  approve: String,
+  draft_e: String,
+  check_e: String,
+  review_e: String,
+  approve_e: String
+});
 
-};
+let DocSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  index_shrot: String,
+  category: { type: String, required: true },
+  revisions: [ RevisionSchema ] 
+});
 
+
+
+module.exports = DocSchema;
+// module.exports.DocList = DocList;
