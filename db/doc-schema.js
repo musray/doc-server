9e480a18967045e965f3bd0d3e58f1803cdd9979
musray <churui.ctec@gmail.com>
2016-07-29 11:35:38 +0800

    working in db directory for REST API

--- db/doc-schema.js
+++ db/doc-schema.js
@@ -1,28 +1,13 @@
 'use strict'
 const mongoose = require('mongoose');
 
-let RevisionSchema = new mongoose.Schema({
-  rev: String,
-  status: String,
-  date: Date,
-  draft: String,
-  check: String,
-  review: String,
-  approve: String,
-  draft_e: String,
-  check_e: String,
-  review_e: String,
-  approve_e: String
+// mongoose schema for an entry of 
+// IED document.
+let Schema = new mongoose.Schema({
+  index: { type: String, required: true },
+  index_short: { type: String },
+  title: { type: String, required: true },
+  rev: { type: String, required: true }
 });
 
-let DocSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  index_shrot: String,
-  category: { type: String, required: true },
-  revisions: [ RevisionSchema ] 
-});
-
-
-
-module.exports = DocSchema;
-// module.exports.DocList = DocList;
+module.exports = Schema;
